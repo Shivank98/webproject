d358ce4f12a1ef64c350844845b628e528dc47b2
Shivank98 <shivankgautam98@gmail.com>
2018-03-07 22:52:04 +0530

    Added Topic.views and count the replies etc

--- boards/views.py
+++ boards/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 # Create your views here.
 
 def home(request):
@@ -12,11 +13,9 @@ def home(request):
 	return render(request, 'home.html', {'boards':boards})
 	
 def board_topics(request, pk):
-	try:
-		board = Board.objects.get(pk = pk)
-	except Board.DoesNotExist:
-		raise Http404
-	return render(request, 'topics.html', {'board':board})
+	board = get_object_or_404(Board, pk=pk)
+	topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+	return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 @login_required
 def new_topic(request, pk):
@@ -42,6 +41,8 @@ def new_topic(request, pk):
 
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
 
 
